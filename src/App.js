5be0f3fe8ee87b7050fbad79626011cdd8912a4c
vikas6577 <vs509411@gmail.com>
2022-06-15 16:38:05 +0530

    basic format of react app

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-import './App.css';
-
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import { Home } from "./components/Home";
+import About from "./components/About";
 function App() {
   return (
-    <div className="App">
-   <h1>hello</h1>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        {/* <Home /> */}
+        <Routes>
+          <Route exact path="/"
+          element={<Home />}/>
+          <Route exact path="/about"
+            element= {<About/>}/>
+        </Routes>
+      </Router>
+    </>
   );
 }
 
 export default App;
-
-
 
 // in package.json in script we wrote a line both:...... using which we can run server and react app simultaneously using single terminal
